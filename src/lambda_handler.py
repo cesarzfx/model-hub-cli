7e29e96ae18029197b5e231d5b73d879cabc1429
cesarzfx <cesarmd931@gmail.com>
2025-11-04 13:12:07 -0500

    fix: update CI workflow to upgrade pip and adjust API Gateway settings for improved routing

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,6 +1,13 @@
 from typing import Any, Dict
+from mangum import Mangum
+from src.api.main import app
 
 
-def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
-    """AWS Lambda handler function."""
-    return {"statusCode": 200, "body": "ok", "headers": {"Content-Type": "text/plain"}}
+# def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
+#     """AWS Lambda handler function."""
+#     return {"statusCode": 200, "body": "ok", "headers": {"Content-Type": "text/plain"}}
+
+handler = Mangum(app)
+
+def lambda_handler(event, context):
+    return handler(event, context)
